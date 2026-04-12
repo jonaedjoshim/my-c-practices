b201b149cb38bd4f53bb0442af53dd0b7b18510c
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-04-12 14:51:29 +0600

    hacker rank problem solvibg

--- helloWorldin.C
+++ helloWorldin.C
@@ -2,7 +2,7 @@
 int main()
 {
     char s[100];
-    scanf("%[^\n]%*c", &s);
+    scanf("\n%[^\n]%*c", &s);
     printf("Hello, World!\n%s", s);
     return 0;
 }
---
+++ sumAndDIfferenceOf2Num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    float c, d;
+    scanf("%d%d", &a, &b);
+    scanf("%f%f", &c, &d);
+    int sum = a + b;
+    int diff = a - b;
+    float sum1 = c + d;
+    float diff1 = c - d;
+    printf("%d %d\n", sum, diff);
+    printf("%.1f %.1f", sum1, diff1);
+    return 0;
+}
